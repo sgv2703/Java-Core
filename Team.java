4abe9c78a4c7b47d9ae005422e925f8021e2d8a7
sgv2703 <83922124+sgv2703@users.noreply.github.com>
2021-11-24 19:10:57 +0300

    Add files via upload

---
+++ Team.java
@@ -0,0 +1,50 @@
+package homeWork1;
+
+import homeWork1.animals.*;
+
+import homeWork1.obstracles.Obstacle;
+
+
+
+public class Team {
+    private String name;
+    private Participant participants[] ;
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+    public Team(String name,Participant ... participantsGiven ) {
+        this.name = name;
+        this.participants = participantsGiven;
+    }
+
+    public void getTeamInfo() {
+        System.out.println("Команда: " + this.name );
+        for (Participant participant : participants) {
+            if (participant instanceof Dog) {
+                System.out.println("Собака " + participant.getName());
+            }
+            if (participant instanceof Cat) {
+                System.out.println("Кот " + participant.getName());
+            }
+        }
+    }
+
+    public void showResults(){
+        for (Participant participant : participants) {
+
+            if (!participant.isOnDistance()) {
+                break;
+            }
+        }
+    }
+    public void doIt(Obstacle obstacle){
+        for (Participant participant : participants) {
+            obstacle.doIt(participant);
+        }
+    }
+
+
+
+}
